61e9dc895f9aa6a832b57f6a6933086f8ce67724
Sidka <stanova.sidonia@gmail.com>
2025-08-03 17:15:14 +0200

    initial commit

---
+++ src/main/java/com/siduska/ehealthwallet/EHealthWalletApplication.java
@@ -0,0 +1,13 @@
+package com.siduska.ehealthwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHealthWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EHealthWalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/siduska/ehealthwallet/EHealthWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siduska.ehealthwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EHealthWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

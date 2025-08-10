54d680655b370f64dc30947afcc76ca9dd9182a7
Sidka <stanova.sidonia@gmail.com>
2025-08-10 23:33:35 +0200

    Implement reviewing of reimbursement pending requests.

---
+++ src/main/java/com/siduska/ehealthwallet/controller/ReimbursementViewController.java
@@ -0,0 +1,25 @@
+package com.siduska.ehealthwallet.controller;
+
+import com.siduska.ehealthwallet.service.ReimbursementService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/requests")
+public class ReimbursementViewController {
+
+    private final ReimbursementService reimbursementService;
+
+    @GetMapping("/pending")
+    public String getAllPendingReimbursements(Model model) {
+        model.addAttribute("pending", reimbursementService.getAllReimbursementsByStatus("PENDING"));
+        return "pending";
+    }
+}

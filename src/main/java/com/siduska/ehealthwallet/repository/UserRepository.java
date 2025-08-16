6fc7fb8c607ad5f8bb4b7041ac91951b728ac137
Sidka <stanova.sidonia@gmail.com>
2025-08-16 07:03:15 +0200

    implement spring security into user api

--- src/main/java/com/siduska/ehealthwallet/repository/UserRepository.java
+++ src/main/java/com/siduska/ehealthwallet/repository/UserRepository.java
@@ -3,8 +3,6 @@ package com.siduska.ehealthwallet.repository;
 import com.siduska.ehealthwallet.entitiy.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(String username);

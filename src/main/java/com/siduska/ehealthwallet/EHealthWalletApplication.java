c84d523e667cc71313371b7eb7d7e1a742430f57
Sidka <stanova.sidonia@gmail.com>
2025-08-17 12:42:03 +0200

    updates for railway deployment

--- src/main/java/com/siduska/ehealthwallet/EHealthWalletApplication.java
+++ src/main/java/com/siduska/ehealthwallet/EHealthWalletApplication.java
@@ -1,22 +1,11 @@
 package com.siduska.ehealthwallet;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(
-        info = @Info(
-                title = "E-health wallet API",
-                version = "1.0",
-                description = "Reimbursement information"
-        ),
-        security = { @SecurityRequirement(name = "ehealthwalletapi") }
-)
 @SecurityScheme(
         name = "ehealthwalletapi",
         scheme = "basic",
---
+++ src/main/java/com/siduska/ehealthwallet/conf/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.siduska.ehealthwallet.conf;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${swagger.server.url}")
+    private String swaggerServerUrl;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("E-health wallet API")
+                        .version("1.0")
+                        .description("Reimbursement information"))
+                .servers(List.of(new Server().url(swaggerServerUrl)));
+    }
+}
